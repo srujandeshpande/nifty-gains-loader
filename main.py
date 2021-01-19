fa3e94302657a927239bc22937567ace575c191f
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-01-19 18:27:33 +0530

    Added discord hook and shifted to NIFTY 500

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import sys
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 path = sys.argv[0][:-7]
 
 nse = Nse()
 
+# Read the holidays file
 with open(path + "holidays.txt") as f:
     holidays = f.readlines()
 
+# Get current date
 ist = pytz.timezone("Asia/Calcutta")
 today = datetime.now(ist)
 curr = today.strftime("%Y-%m-%d")
@@ -34,10 +41,11 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-# Individual Quotes
-with open(path + "nifty50.txt") as f:
+# Get Individual Tickers from file
+with open(path + "data/nifty500.txt") as f:
     nifty50 = f.readlines()
 
+# Get Metadata
 doc = db.collection("meta").document("dates").get()
 if doc.exists:
     data = doc.to_dict()
@@ -58,24 +66,43 @@ else:
     data = {"dates": [curr]}
 db.collection("meta").document("dates").set(data)
 
+# Init empty dict for NIFTY 500 data of that day
+nifty500_data = {}
 
-# Nifty Quote
+# Get Quote for each Ticker
 for j in nifty50:
     ticker = j.strip()
     quote = nse.get_quote(ticker)
     data = {
-        "open": quote["open"],
-        "close": quote["closePrice"],
-        "pChange": quote["pChange"],
+        "open": float(quote["open"]),
+        "close": float(quote["closePrice"]),
+        "pChange": float(quote["pChange"]),
         "date": curr,
     }
-    db.collection(ticker).document(curr).set(data)
+    nifty500_data[ticker] = data
+    # db.collection(ticker).document(curr).set(data)
 
-quote = nse.get_index_quote("NIFTY 50")
+# Sort the data by pChange
+sorted_p = sorted(nifty500_data.items(), key=lambda x: x[1]['pChange'], reverse=True)
+content = "Today's Max Gainers\n\n"
+for i in sorted_p[:20]:
+    content += i[0] + ": " + str(i[1]['pChange']) + "\n"
+
+# Send Discord message
+url = os.getenv("DISCORD_HOOK")
+myobj = {"content": content}
+x = requests.post(url, data=myobj)
+
+# Get Index quote
+quote = nse.get_index_quote("NIFTY 500")
 data = {
     "lastPrice": quote["lastPrice"],
     "change": quote["change"],
     "pChange": quote["pChange"],
     "date": curr,
 }
-db.collection("NIFTY50").document(curr).set(data)
+# Update index quote in database
+db.collection("NIFTY500").document(curr).set(data)
+
+for i in sorted_p:
+    db.collection(i[0]).document(curr).set(i[1])
