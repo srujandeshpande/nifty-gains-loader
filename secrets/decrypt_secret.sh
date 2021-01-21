7c5fab672a295325e8d318f6823b79f35abed088
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-01-21 22:49:35 +0530

    Modify path in secret

--- secrets/decrypt_secret.sh
+++ secrets/decrypt_secret.sh
@@ -5,4 +5,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIRESTORE_PASSPHRASE" \
---output ../firestore-access.json firestore-access.json.gpg
+--output ./firestore-access.json ./secrets/firestore-access.json.gpg
